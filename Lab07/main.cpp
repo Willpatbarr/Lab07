38ce1155f03fedfcdaa91da88933a68fc5e8ad30
Willpatbarr <145700970+Willpatbarr@users.noreply.github.com>
2025-02-22 02:30:57 -0700

    ticket 5 completed

--- Lab07/main.cpp
+++ Lab07/main.cpp
@@ -7,7 +7,43 @@ using namespace std;
 
 // Set Global Variables
 #define WEIGHT   46.7   // Weight in KG
-#define TIME     0.01           // the amount of time in seconds used to increment
+#define TIME     0.01   // the amount of time in seconds used to increment
+#define DRAG     0.3    // drag coeffecient as a constant
+#define AIR      0.6    // the density of air as a constant
+
+/***********************************************
+ * INTERPOLATE GRAVITY VALUE
+ * Interpolate for the value of gravity at a given altitude
+ * INPUT
+ *     altitude
+ * OUTPUT
+ *     g: the value of gravity linearly interpolated
+ ***********************************************/
+double interpolateGravity(double altitude)
+{
+   // Gravity-altitude table
+      const double altitudes[] = {0, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 15000, 20000, 25000, 30000, 40000, 50000, 60000, 70000, 80000};
+      const double gravities[] = {9.807, 9.804, 9.801, 9.797, 9.794, 9.791, 9.788, 9.785, 9.782, 9.779, 9.776, 9.761, 9.745, 9.730, 9.715, 9.684, 9.654, 9.624, 9.594, 9.564};
+
+   // Find the two value of altitude above and below the input altitude
+   for (int i = 0; i < 19; i++) // iterates 20 times becuase of size of the arrays
+   {
+      if (altitude >= altitudes[i] && altitude <= altitudes[i + 1])
+      {
+         // get the values
+         double g0 = gravities[i];
+         double g1 = gravities[i + 1];
+         double y0 = altitudes[i];
+         double y1 = altitudes[i + 1];
+         
+         //perform the interpolation equation
+         return g0 + ((g1 - g0) * (altitude - y0) / (y1 - y0));
+      }
+   }
+   
+   return 9.807;
+}
+
 
 /***********************************************
  * COMPUTE HORIZONTAL COMPONENT
@@ -99,6 +135,7 @@ int main()
    double x = 0.0; // set to 0.0 as default
    double y = 0.0; // set to 0.0 as default
    double t = 0.0; // used to keep track of hang time
+   double g = 9.807; // initial value at sea level
    
    // convert degrees to radians
    double angleInRad = radFromDeg(angle);
@@ -117,21 +154,24 @@ int main()
       // update the position
       x = x + dx * TIME;
       y = y + dy * TIME;
+      
+      // update gravity
+      g = interpolateGravity(y);
 
       // update the vertical velocity component with acceleration due to gravity
-      dy = dy + (-9.8) * TIME;
+      dy = dy + (-g) * TIME;
       
       // update the hangtime
       t += TIME;
 
       // display updated values
-      cout << "Time: " << t << " | Distance: " << x << " | Altitude: " << y << " | Hang Time: " << t << endl;
+      cout << "Time: " << t << " | Distance: " << x << " | Altitude: " << y << endl;
    }
    
    cout << " --------------------------------------------" << endl;
    cout << "Ground Impact" << endl;
    cout << "Final Values" << endl;
-   cout << "Time: " << t << " | Distance: " << x << " | Altitude: " << y << " | Hang Time: " << t << endl;
+   cout << "Distance: " << x << " | Altitude: " << y << " | Hang Time: " << t << endl;
    
    return 0;
 }
