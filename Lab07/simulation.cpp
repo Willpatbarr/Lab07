ac67a821e49ab474ab8e83aff154dcef747042d2
pmark2047 <peytonmarkus24@gmail.com>
2025-03-05 19:54:52 -0700

    Added spacing to Fire() for better readability

--- Lab07/simulation.cpp
+++ Lab07/simulation.cpp
@@ -34,56 +34,56 @@ void Simulation::fire()
    do
    {
       double radius = 154.89 / 2;
-      cout << "radius: " << radius << endl;
+      cout << "   radius: " << radius;
       
       double y = pvt.pt.getY();
-      cout << "y: " << y << endl;
+      cout << "   y: " << y;
 
       Direction direction = pvt.v.getDirection();
 
       double speed = pvt.v.getSpeed();
-      cout << "speed: " << speed << endl;
+      cout << "   speed: " << speed;
 
       double x = pvt.pt.getX();
-      cout << "x: " << x << endl;
+      cout << "   x: " << x;
 
       
       //compute speed of sound
       double mach = machFromAltitude(y);
-      cout << "mach: " << mach << endl;
+      cout << "   mach: " << mach;
 
       
       //compute mach of shell
       double machOfShell = speed / mach;
-      cout << "machOfShell: " << machOfShell << endl;
+      cout << "   machOfShell: " << machOfShell;
 
       
       //compute air density
       double density = densityFromAltitude(y);
-      cout << "density: " << density << endl;
+      cout << "   density: " << density;
 
       
       //compute drag coefficient
       double drag = dragFromMach(machOfShell);
-      cout << "drag: " << drag << endl;
+      cout << "   drag: " << drag;
 
       
       //compute forces on shell
       double dragForce = forceFromDrag(density, drag, radius, pvt.v.getSpeed());
-      cout << "dragForce: " << dragForce << endl;
+      cout << "   dragForce: " << dragForce;
 
       
       //compute acceleration
       double acceleration = accelerationFromForce(dragForce, shellMass);
-      cout << "acceleration: " << acceleration << endl;
+      cout << "   acceleration: " << acceleration;
 
       
       //compute velocity components
       double dx = computeHorizontalComponent(direction.getDegrees(), speed);
-      cout << "dx: " << dx << endl;
+      cout << "dx: " << dx;
 
       double dy = computeVerticalComponent(direction.getDegrees(), speed);
-      cout << "dy: " << dy << endl;
+      cout << "   dy: " << dy;
 
       
       pvt.v.setDX(dx);
@@ -91,26 +91,26 @@ void Simulation::fire()
       
       //compute acceleration components
       double ddx = computeHorizontalComponent(direction.getDegrees(), acceleration) * -1;
-      cout << "ddx: " << ddx << endl;
+      cout << "   ddx: " << ddx;
 
       double ddy = (computeVerticalComponent(direction.getDegrees(), acceleration) + gravityFromAltitude(y)) * -1;
-      cout << "ddy: " << ddy << endl;
+      cout << "   ddy: " << ddy;
 
       
       //compute positions
       x = computeDistance(x, dx, interval, ddx);
-      cout << "x: " << x << endl;
+      cout << "   x: " << x;
 
       y = computeDistance(y, dy, interval, ddy);
-      cout << "y: " << y << endl;
+      cout << "   y: " << y;
 
       
       //recompute velocity components using v1 = v0 + a * t
       dx = dx + ddx * interval;
-      cout << "new dx: " << dx << endl;
+      cout << "   new dx: " << dx;
 
       dy = dy + ddy * interval;
-      cout << "new dy: " << dy << endl;
+      cout << "   new dy: " << dy << "\n" << endl;
 
       
       //update pvt
